fad1fe3e2bbdc202649ad206e9e9ece960639aa7
Alejandro Lazcano <2524160080@e.uttecamac.edu.mx>
2026-09-20 13:30:18 -0600

    feat: implementa la interfaz de canciones más escuchadas

--- src/app/musica/musica.ts
+++ src/app/musica/musica.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Cancion} from '../interfaces/cancion.interface';
 
 @Component({
   imports: [],
@@ -6,4 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './musica.css',
   templateUrl: './musica.html',
 })
-export class Musica {}
+export class Musica {
+  canciones: Cancion[] = [
+  {
+    id: 1,
+    titulo: 'ni pedo',
+    artista: 'Peso Pluma, Tito Double P',
+    imagen: 'img/ni-pedo.jpg',
+    reproducciones: 150
+  },
+  {
+    id: 2,
+    titulo: 'Bien Bebé',
+    artista: 'El Malilla',
+    imagen: 'img/bien-bebe.jpg',
+    reproducciones: 120
+  },
+  {
+    id: 3,
+    titulo: 'Quisiera Saber',
+    artista: 'Los Daniels, Natalia Lafourcade',
+    imagen: 'img/quisiera-saber.jpg',
+    reproducciones: 100
+  }
+];
+}
