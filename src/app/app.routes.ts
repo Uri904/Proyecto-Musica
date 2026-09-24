2f4181becb0ae8c1f96367193ca8ce1b763afdc3
Uriel Tapia <isai8.tapia@gmail.com>
2026-09-23 23:04:16 -0600

    Botón registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
+import { Registro } from './registro/registro';
 import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: Login },
+  { path: 'registro', component: Registro },
   { 
     path: 'musica', 
     loadComponent: () => import('./musica/musica').then(m => m.Musica),
